eb76d22b96c1f4d229b18d813f4f3fec8f16f2a6
mufasa1976 <mufasa1976@coolstuff.software>
2022-11-18 16:32:29 +0100

    some minor bugfixes

--- server/src/main/java/io/github/mufasa1976/calcmaster/controllers/IndexController.java
+++ server/src/main/java/io/github/mufasa1976/calcmaster/controllers/IndexController.java
@@ -1,27 +1,21 @@
 package io.github.mufasa1976.calcmaster.controllers;
 
+import io.github.mufasa1976.calcmaster.config.WebMvcConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import io.github.mufasa1976.calcmaster.config.WebMvcConfiguration;
 
 import java.util.Arrays;
 import java.util.Locale;
 
 @Controller
 public class IndexController {
-  private static final String DEFAULT_LANDING_PAGE = "en/index";
+  private static final String DEFAULT_REDIRECT = "redirect:/en";
 
   @GetMapping
   public String landingPage(final Locale locale) {
-    return "redirect:/" + locale.getCountry();
-  }
-
-  @GetMapping("{language}")
-  public String localeSpecificLandingPage(@PathVariable("language") final String language) {
-    if (Arrays.asList(WebMvcConfiguration.OFFERED_ANGULAR_LANGUAGES).contains(language)) {
-      return language + "/index";
+    if (Arrays.asList(WebMvcConfiguration.OFFERED_ANGULAR_LANGUAGES).contains(locale.getLanguage())) {
+      return "redirect:/" + locale.getLanguage();
     }
-    return DEFAULT_LANDING_PAGE;
+    return DEFAULT_REDIRECT;
   }
 }
--- server/src/main/java/io/github/mufasa1976/calcmaster/services/Calculator.java
+++ server/src/main/java/io/github/mufasa1976/calcmaster/services/Calculator.java
@@ -1,10 +1,11 @@
 package io.github.mufasa1976.calcmaster.services;
 
-import lombok.extern.slf4j.Slf4j;
 import io.github.mufasa1976.calcmaster.ApplicationProperties;
 import io.github.mufasa1976.calcmaster.dtos.Calculation;
 import io.github.mufasa1976.calcmaster.records.CalculationProperties;
 import io.github.mufasa1976.calcmaster.records.Calculations;
+import io.github.mufasa1976.calcmaster.suppliers.*;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
 import java.util.Optional;
@@ -25,20 +26,22 @@ public class Calculator {
     this.applicationProperties = applicationProperties;
 
     this.random = new Random();
-    this.calculationSuppliers = calculationProperties.operators()
-                                                     .stream()
-                                                     .map(operator -> switch (operator) {
-                                                       case ADD -> new AdditionSupplier(random, calculationProperties.toggleHide(), applicationProperties.getMaxTriesToGetDistinctOperationTuples(),
-                                                           calculationProperties.additionProperties(), applicationProperties.getMaxTriesToFindSumOfAdditionNotEqualToSecondAddend());
-                                                       case SUBTRACT -> new SubtractionSupplier(random, calculationProperties.toggleHide(),
-                                                           applicationProperties.getMaxTriesToGetDistinctOperationTuples(), calculationProperties.subtractionProperties());
-                                                       case MULTIPLY -> new MultiplicationSupplier(random, calculationProperties.toggleHide(),
-                                                           applicationProperties.getMaxTriesToGetDistinctOperationTuples(), calculationProperties.multiplicationProperties());
-                                                       case DIVIDE -> new DivisionSupplier(random, calculationProperties.toggleHide(),
-                                                           applicationProperties.getMaxTriesToGetDistinctOperationTuples(), calculationProperties.divisionProperties());
-                                                     })
-                                                     .peek(CalculationSupplier::init)
-                                                     .collect(Collectors.toList());
+    this.calculationSuppliers =
+        calculationProperties.operators()
+                             .stream()
+                             .map(operator -> switch (operator) {
+                               case ADD ->
+                                   new AdditionSupplier(random, calculationProperties.toggleHide(), applicationProperties.getMaxTriesToGetDistinctOperationTuples(),
+                                       calculationProperties.additionProperties(), applicationProperties.getMaxTriesToFindSumOfAdditionNotEqualToSecondAddend());
+                               case SUBTRACT -> new SubtractionSupplier(random, calculationProperties.toggleHide(),
+                                   applicationProperties.getMaxTriesToGetDistinctOperationTuples(), calculationProperties.subtractionProperties());
+                               case MULTIPLY -> new MultiplicationSupplier(random, calculationProperties.toggleHide(),
+                                   applicationProperties.getMaxTriesToGetDistinctOperationTuples(), calculationProperties.multiplicationProperties());
+                               case DIVIDE -> new DivisionSupplier(random, calculationProperties.toggleHide(),
+                                   applicationProperties.getMaxTriesToGetDistinctOperationTuples(), calculationProperties.divisionProperties());
+                             })
+                             .peek(CalculationSupplier::init)
+                             .collect(Collectors.toList());
   }
 
   public Optional<Calculations> calculate() {
